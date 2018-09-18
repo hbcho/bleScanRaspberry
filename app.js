8c0105de6356b6c7a4f10dbb8ded2b50092a35dc
Mateus Capistrano <mateusplc@hotmail.com>
2018-09-18 13:10:07 +0000

    Posting data into database

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 const Noble = require("noble");
 const BeaconScanner = require("node-beacon-scanner");
 
+var request = require('request');
+
 var scanner = new BeaconScanner();
 
-scanner.onadvertisement = (advertisement) => {
-	var beacon = advertisement["iBeacon"];
-	beacon.rssi = advertisement["rssi"];
-	console.log(JSON.stringify(beacon, null, "   "));
+var jsonResult;
+
+function scanController(){
+
+	scanner.startScan().then(() => {
+		console.log("Scanning has started..");
+	}).catch(error => {
+		console.error(error)
+	})
+
+	scanner.stopScan();
 }
 
-scanner.startScan().then(() => {
-	console.log("Scanning has started..");
-}).catch(error => {
-	console.error(error)
-})
+setInterval(scanController, 1000);
+
+scanner.onadvertisement = (advertisement) => {
+	
+	var beacon = advertisement["iBeacon"];
+	var rssi = advertisement["rssi"];
+	
+	jsonResult = beacon;
+
+	if(jsonResult != "undefined") {
+		
+		request.post({
+		
+			url: 'https://botcare.herokuapp.com/beacons',
+			json: jsonResult
+		},
+	
+		function (err, httpResponse, body){
+			console.log(body);
+		});
+	}			
+}
